78a3c21c9e616eac1d73faa10fe7532804190515
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-11-15 21:49:42 -0500

    start front-end

---
+++ front_end/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <p>&copy; G&#38;G Tax Services</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ front_end/src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Search(props) {
+  return (
+    <div className="search-component">
+      <input className="search-input" />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default Search;
